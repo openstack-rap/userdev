cdf9ecd84e9e66c34b18a1f6593e5050c549a7aa
openstack-rap <128293768+openstack-rap@users.noreply.github.com>
2023-04-01 14:52:04 +0530

    Update and rename demo2.sh to userdev.sh

---
+++ userdev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+sudo -u stack bash /snap/userdev/x1/tools/two.sh
+
